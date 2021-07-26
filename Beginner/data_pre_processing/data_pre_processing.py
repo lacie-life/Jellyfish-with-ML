8627c41219a1ca4438ef22648261a68651fdd6c8
lacie-life <55802547+lacie-life@users.noreply.github.com>
2021-07-26 20:46:45 +0700

    update data preprocessing example

--- Beginner/data_pre_processing/data_pre_processing.py
+++ Beginner/data_pre_processing/data_pre_processing.py
@@ -36,4 +36,26 @@ cols2 = ['ApplicatIncome', 'CoapplicantIncome', 'LocanAmount']
 dt[cols2] = dt[cols2].fillna(dt.mean())
 dt.isnull().sum(axis=0)
 
+# Label Encodeing using Python
+dt.dtypes
+
+# Changes the datatypes
+dt[cols] = dt[cols].astype('category')
+
+for columns in cols:
+    dt[columns] = dt[columns].cat.codes
+
+# Create Dummy variables or Hot encoding
+df2 = data_set.drop(['Loan_ID'], axis=1)
+df2 = pd.get_dummies(df2)
+
+# Split the dât vertically into X and Y
+X = df2.iloc[:, :-1]  # Select all yhe columns execpt loan_status
+Y = df2.iloc[:, -1]   # Select only the loan_status
+
+
+# Split the dataset by rows
+from sklearn.model_selection import train_test_split
+
+x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size=0.2, random_state=1234)
 
