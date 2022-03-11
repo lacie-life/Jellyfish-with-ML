205ea9f625af8a0d4cf02c9e104085403b6e7363
lacie-life <00sao00ios00@gmail.com>
2022-03-11 14:01:16 +0700

    [Update] tensorRT example

---
+++ Course/TensorRT/samples/MNIST/build/CMakeFiles/mnist_sample.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mnist_sample.dir/sampleMNIST.cpp.o"
+  "mnist_sample"
+  "mnist_sample.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mnist_sample.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
